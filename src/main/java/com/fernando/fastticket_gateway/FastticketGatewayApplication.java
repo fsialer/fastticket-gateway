7429f6880db73a6d5e932e49252fb7fc0891d760
Fernando <asialer05@hotmail.com>
2025-11-02 19:46:12 -0500

    first commit

---
+++ src/main/java/com/fernando/fastticket_gateway/FastticketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.fastticket_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastticketGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastticketGatewayApplication.class, args);
+	}
+
+}
